5944da9c9d624097222559bdf515dbe1be714d0a
Sterling <sterlingbarton81@gmail.com>
2023-05-04 09:10:25 -0600

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_list = [n for n in num_list if n % 2 == 0]
+    return new_list
+
 
 def make_exclamation(sentence_list):
-    pass
+    excited_list = [(f'{sentence}!') for sentence in sentence_list]
+    return excited_list
